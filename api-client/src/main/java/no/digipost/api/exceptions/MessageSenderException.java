60eb0ce83c457d714500ea2858d48a42ded8b27c
Stefan Magnus Landrø <stefan.landro@gmail.com>
2014-07-08 08:14:01 +0200

    Oppdaterer message sender exception til også å kunne ta inn en throwable

--- api-client/src/main/java/no/digipost/api/exceptions/MessageSenderException.java
+++ api-client/src/main/java/no/digipost/api/exceptions/MessageSenderException.java
@@ -20,5 +20,8 @@ public abstract class MessageSenderException extends RuntimeException {
 	public MessageSenderException(String message) {
 		super(message);
 	}
+	public MessageSenderException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
--- api-client/src/main/java/no/digipost/api/exceptions/MessageSenderIOException.java
+++ api-client/src/main/java/no/digipost/api/exceptions/MessageSenderIOException.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 public class MessageSenderIOException extends MessageSenderException {
 
 	public MessageSenderIOException(String message, IOException e) {
-		super(message);
+		super(message, e);
 	}
 
 }

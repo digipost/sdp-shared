cf32a429b140f8f633456e105ae384808794e770
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-06-05 21:04:45 +0200

    Legger til lisensheadere

--- api-commons/src/test/java/no/digipost/api/representations/ObjectMother.java
+++ api-commons/src/test/java/no/digipost/api/representations/ObjectMother.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) Posten Norge AS
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.digipost.api.representations;
 
 import org.w3.xmldsig.DigestMethod;
@@ -9,7 +24,7 @@ import java.util.List;
 
 public class ObjectMother {
 
-	public static Reference getReference(){
+	public static Reference getReference() {
 		Reference reference = new Reference();
 		reference.setURI("#id-f2ecf3b2-101e-433b-a30d-65a9b6779b5a");
 
--- api-commons/src/test/java/no/digipost/api/representations/ReferanseTest.java
+++ api-commons/src/test/java/no/digipost/api/representations/ReferanseTest.java
@@ -1,12 +1,22 @@
+/**
+ * Copyright (C) Posten Norge AS
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.digipost.api.representations;
 
 import org.junit.Test;
-import org.w3.xmldsig.DigestMethod;
 import org.w3.xmldsig.Reference;
-import org.w3.xmldsig.Transform;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -20,7 +30,7 @@ public class ReferanseTest {
 	}
 
 	@Test
-	public void  testGetUnmarshaled() {
+	public void testGetUnmarshaled() {
 		Reference reference = ObjectMother.getReference();
 		Referanse referanse = Referanse.builder(reference).build();
 

2afb32caa22c8fc182d6746358b145e3dbab1e21
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-06-06 09:51:41 +0200

    Drar ut KanBekreftesSomBehandletKvittering til MessageSender.bekreft

--- api-client/src/main/java/no/digipost/api/handlers/BekreftelseSender.java
+++ api-client/src/main/java/no/digipost/api/handlers/BekreftelseSender.java
@@ -17,28 +17,34 @@ package no.digipost.api.handlers;
 
 import no.digipost.api.interceptors.steps.AddReferencesStep;
 
-import no.digipost.api.representations.EbmsApplikasjonsKvittering;
+import no.digipost.api.representations.KanBekreftesSomBehandletKvittering;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.client.core.WebServiceMessageCallback;
+import org.w3.xmldsig.Reference;
 
 import javax.xml.transform.TransformerException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BekreftelseSender extends EbmsContextAware implements WebServiceMessageCallback {
 
-	private final EbmsApplikasjonsKvittering appKvittering;
+	private final KanBekreftesSomBehandletKvittering kanBekreftesSomBehandletKvittering;
 	private final Jaxb2Marshaller jaxb2Marshaller;
 
-	public BekreftelseSender(final EbmsApplikasjonsKvittering appKvittering, final Jaxb2Marshaller jaxb2Marshaller) {
-		this.appKvittering = appKvittering;
+	public BekreftelseSender(final KanBekreftesSomBehandletKvittering kanBekreftesSomBehandletKvittering, final Jaxb2Marshaller jaxb2Marshaller) {
+		this.kanBekreftesSomBehandletKvittering = kanBekreftesSomBehandletKvittering;
 		this.jaxb2Marshaller = jaxb2Marshaller;
 	}
 
 	@Override
 	public void doWithMessage(final WebServiceMessage message) throws IOException, TransformerException {
-		ebmsContext.addRequestStep(new AddReferencesStep(jaxb2Marshaller, appKvittering.messageId, appKvittering.references));
+		List<Reference> references = new ArrayList<Reference>();
+		references.add(kanBekreftesSomBehandletKvittering.getReferanse().getUnmarshaled());
+
+		ebmsContext.addRequestStep(new AddReferencesStep(jaxb2Marshaller, kanBekreftesSomBehandletKvittering.getMeldingsId(), references));
 	}
 
 }

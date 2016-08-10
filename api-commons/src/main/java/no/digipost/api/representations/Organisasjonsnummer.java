51935dccfddfad925f08a19152d7b07aef105828
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-08-10 13:46:06 +0200

    Moves finals in Organisasjonsnummer to top and translates `canditates`

--- api-commons/src/main/java/no/digipost/api/representations/Organisasjonsnummer.java
+++ api-commons/src/main/java/no/digipost/api/representations/Organisasjonsnummer.java
@@ -29,44 +29,39 @@ import static java.util.Arrays.asList;
 
 public final class Organisasjonsnummer {
 
-	public static final String ISO6523_ACTORID = PMode.PARTY_ID_TYPE;
-	public static final String ISO6523_ACTORID_OLD = "iso6523-actorid-upis";
+    public static final String ISO6523_ACTORID = PMode.PARTY_ID_TYPE;
+    public static final String ISO6523_ACTORID_OLD = "iso6523-actorid-upis";
+    private static final String COUNTRY_CODE_ORGANIZATION_NUMBER_NORWAY = "9908";
+    private static final Pattern ORGANIZATION_NUMBER_PATTERN = Pattern.compile("^(" + COUNTRY_CODE_ORGANIZATION_NUMBER_NORWAY + ":)?([0-9]{9})$");
+    private final String organisasjonsnummer;
 
-	public static boolean erGyldig(String organisasjonsnummer) {
-	    return ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer).matches();
-	}
 
-	public static Optional<Organisasjonsnummer> hvisGyldig(String organisasjonsnummer) {
-	    return Optional.of(ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer))
-	            .filter(Matcher::matches)
-	            .map(Organisasjonsnummer::new);
-	}
+    private Organisasjonsnummer(MatchResult matchedOrganisasjonsnummer) {
+        int groupOfOrganizationNumber = matchedOrganisasjonsnummer.groupCount();
+        this.organisasjonsnummer = matchedOrganisasjonsnummer.group(groupOfOrganizationNumber);
+    }
 
-	public static Organisasjonsnummer of(String organisasjonsnummer) {
-	    Matcher matcher = ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer);
-	    if (matcher.matches()) {
-	        return new Organisasjonsnummer(matcher);
-	    } else {
-	        throw new IllegalArgumentException(
+    public static boolean erGyldig(String organisasjonsnummer) {
+        return ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer).matches();
+    }
+
+    public static Optional<Organisasjonsnummer> hvisGyldig(String organisasjonsnummer) {
+        return Optional.of(ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer))
+                .filter(Matcher::matches)
+                .map(Organisasjonsnummer::new);
+    }
+
+    public static Organisasjonsnummer of(String organisasjonsnummer) {
+        Matcher matcher = ORGANIZATION_NUMBER_PATTERN.matcher(organisasjonsnummer);
+        if (matcher.matches()) {
+            return new Organisasjonsnummer(matcher);
+        } else {
+            throw new IllegalArgumentException(
                     "Ugyldig organisasjonsnummer. Forventet format er ISO 6523, men fikk følgende nummer: '" +
-                    organisasjonsnummer + "'. Organisasjonsnummeret skal være 9 siffer og kan prefikses med " +
-                    "landkode 9908. Eksempler på dette er '9908:984661185' og '984661185'.");
-	    }
-	}
-
-
-
-
-	private static final String COUNTRY_CODE_ORGANIZATION_NUMBER_NORWAY = "9908";
-
-	private static final Pattern ORGANIZATION_NUMBER_PATTERN = Pattern.compile("^(" + COUNTRY_CODE_ORGANIZATION_NUMBER_NORWAY +":)?([0-9]{9})$");
-
-	private final String organisasjonsnummer;
-
-	private Organisasjonsnummer(MatchResult matchedOrganisasjonsnummer) {
-	    int groupOfOrganizationNumber = matchedOrganisasjonsnummer.groupCount();
-	    this.organisasjonsnummer = matchedOrganisasjonsnummer.group(groupOfOrganizationNumber);
-	}
+                            organisasjonsnummer + "'. Organisasjonsnummeret skal være 9 siffer og kan prefikses med " +
+                            "landkode 9908. Eksempler på dette er '9908:984661185' og '984661185'.");
+        }
+    }
 
     public String getOrganisasjonsnummer() {
         return organisasjonsnummer;
@@ -80,12 +75,12 @@ public final class Organisasjonsnummer {
         return hvisGyldig(organisasjonsnummerString).filter(this::equals).isPresent();
     }
 
-    public boolean erEnAv(Organisasjonsnummer ... candidates) {
-        return erEnAv(asList(candidates));
+    public boolean erEnAv(Organisasjonsnummer... kandidater) {
+        return erEnAv(asList(kandidater));
     }
 
-    public boolean erEnAv(Collection<Organisasjonsnummer> candidates) {
-        return candidates.contains(this);
+    public boolean erEnAv(Collection<Organisasjonsnummer> kandidater) {
+        return kandidater.contains(this);
     }
 
     @Override

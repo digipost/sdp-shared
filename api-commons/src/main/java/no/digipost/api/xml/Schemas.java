40903d337a056e4f4d7ecc4f30046ddd747d0418
Sindre Nordbø <sindre.nordbo@bekk.no>
2018-01-11 13:16:48 +0100

    Støtte for «lenke utenfor brev» som «data-dokument»
    Oppdaterer begrepskatalogen som inneholder XSD for `Lenke`.
    Tilgjengeliggjør XSD som `Resource` for konsumenter.

--- api-commons/src/main/java/no/digipost/api/xml/Schemas.java
+++ api-commons/src/main/java/no/digipost/api/xml/Schemas.java
@@ -14,9 +14,11 @@ public class Schemas {
     public static final ClassPathResource XADES_SCHEMA = new ClassPathResource("etsi/XAdES.xsd");
     public static final ClassPathResource ASICE_SCHEMA = new ClassPathResource("asic-e/ts_102918v010201.xsd");
 
+    public static final ClassPathResource LENKE_SCHEMA = new ClassPathResource("utvidelser/lenke.xsd");
+
     public static Resource[] allSchemaResources() {
         return new Resource[]{
-                SDP_SCHEMA, SDP_MANIFEST_SCHEMA, SBDH_SCHEMA, EBMS_SCHEMA, XMLDSIG_SCHEMA, XADES_SCHEMA, ASICE_SCHEMA
+                SDP_SCHEMA, SDP_MANIFEST_SCHEMA, SBDH_SCHEMA, EBMS_SCHEMA, XMLDSIG_SCHEMA, XADES_SCHEMA, ASICE_SCHEMA, LENKE_SCHEMA
         };
     }
 

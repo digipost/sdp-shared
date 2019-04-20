696ee6e0e59b50ad1af5aeb4983347d29d5269be
Rune Flobakk <flobakk@gmail.com>
2019-04-20 23:44:08 +0200

    Remove unused Spring-based xpath util

--- api-commons/src/main/java/no/digipost/api/xml/XpathUtil.java
+++ api-commons/src/main/java/no/digipost/api/xml/XpathUtil.java
@@ -2,31 +2,14 @@ package no.digipost.api.xml;
 
 import org.jaxen.JaxenException;
 import org.jaxen.dom.DOMXPath;
-import org.springframework.xml.xpath.Jaxp13XPathTemplate;
-import org.springframework.xml.xpath.NodeMapper;
-import org.w3c.dom.DOMException;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-import javax.xml.transform.Source;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class XpathUtil {
-
-    private static final NodeMapper<Node> returnNode = new NodeMapper<Node>() {
-        @Override
-        public Node mapNode(final Node node, final int nodeNum) throws DOMException {
-            return node;
-        }
-    };
-
-    public static List<Node> evaluateXpath(final String expression, final Source source) {
-        Jaxp13XPathTemplate template = new Jaxp13XPathTemplate();
-        template.setNamespaces(getXpathNamespaces());
-        return template.evaluate(expression, source, returnNode);
-    }
 
     @SuppressWarnings("unchecked")
     public static List<Node> getDOMXPath(final String expression, final Element element) {
